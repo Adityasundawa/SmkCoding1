90e9ee894bb7a0f3583d869b44c08e213546b9a6
Adityasundawa <adityasundawa.co@gmail.com>
2020-05-05 02:44:51 +0700

    Tambah Layouting Umur + Layouting Profile

---
+++ app/src/main/java/com/adityasundawa/id/ProfileActivity.kt
@@ -0,0 +1,13 @@
+package com.adityasundawa.id
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+    }
+}
